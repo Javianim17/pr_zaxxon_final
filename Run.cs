ea5e2829b2983f8efa93d96e8f5cb594929d78f4
Javianim17 <91114999+Javianim17@users.noreply.github.com>
2021-11-09 12:08:53 +0100

    Terreno Movement

--- Run.cs
+++ Run.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Run : MonoBehaviour
 {
-    float speed = 10f;
+    float speed = 30f;
     // Start is called before the first frame update
     void Start()
     {

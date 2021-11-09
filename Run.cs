05786c83b47f2dcf799e3730e0b2ed8e31703036
Javianim17 <91114999+Javianim17@users.noreply.github.com>
2021-11-09 12:08:01 +0100

    Terreno Movement

---
+++ Run.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour
+{
+    float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = 20f;
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}

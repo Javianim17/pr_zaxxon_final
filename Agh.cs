ce30763a2df45681d99043799c5067436a58c4d7
Javianim17 <91114999+Javianim17@users.noreply.github.com>
2021-11-09 12:00:30 +0100

    Nave movement

---
+++ Agh.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agh : MonoBehaviour
+{
+    public float speedH;
+    public float speedV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float desplaH = speedH * Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * desplaH);
+
+        float desplaV = speedV * Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * Time.deltaTime * desplaV);
+    }
+}
